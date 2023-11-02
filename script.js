25552391377ec2ff34dfafd7a79581579ee071a2
Rakesh Pawar <125602231+RakeshDP@users.noreply.github.com>
2023-11-03 03:28:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+const URL = document.getElementById("url");
+const nameInput = document.getElementByID("name")
+const yearINput = document.getElementByID("year")
+
+function handleSubmit(event){
+	event.preventDefault()
+	const name= nameInput.value;
+	const year = yearINput.value;
+	
+	const queryString = `?name=${name}&year=${year}`;
+	URL.textContent = `https://localhost:8080/${queryString}`
+}
